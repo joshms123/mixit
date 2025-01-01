85bbbc6b73ad60a572176e16219bda68d8276b1d
Josh S <josh.stirland14@gmail.com>
2025-01-01 22:49:32 +0000

    Add auto-export prefix functionality with tests

--- mixit/decorators.py
+++ mixit/decorators.py
@@ -4,15 +4,15 @@ from typing import Callable, TypeVar, Any, Optional
 F = TypeVar('F', bound=Callable[..., Any])
 
 def export(method: Optional[F] = None) -> F:
-	"""Decorator to mark methods that should be exported to the Mixer namespace."""
-	def decorator(func: F) -> F:
-		@wraps(func)
-		def wrapper(self, *args, **kwargs):
-			return func(self, *args, **kwargs)
-		
-		wrapper._is_exported = True
-		return wrapper
-	
-	if method is None:
-		return decorator
-	return decorator(method)
+        """Decorator to mark methods that should be exported to the Mixer namespace."""
+        def decorator(func: F) -> F:
+                @wraps(func)
+                def wrapper(self, *args, **kwargs):
+                        return func(self, *args, **kwargs)
+                
+                wrapper._is_exported = True
+                return wrapper
+        
+        if method is None:
+                return decorator
+        return decorator(method)
